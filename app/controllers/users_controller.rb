d500cb8e6b598ebbf81fede38baf078090d73529
Alieu Baldeh <alieu.dev@gmail.com>
2021-09-24 01:45:01 -0400

    complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+    # GET users/:id
+    def show
+        user = User.find_by(id: user_params[:id])
+        render json: user, include: :items
+    end
+
+    private 
+    def user_params 
+        params.permit(:id, :username, :city, :items)
+    end
 end
